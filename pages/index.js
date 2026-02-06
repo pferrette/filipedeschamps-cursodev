281f38eafe29f2d05d1cc0ed6a56f56a14ca82c4
Pedro Ferrette <97296472+pferrette@users.noreply.github.com>
2026-02-06 18:42:39 +0000

    altera texto do pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>SE MOLEDOY, IMAGINA DURO </h1>;
+  return <h1>VOCÊ ESTÁ SALVO MEU GAROTO, AQUI VOCÊ NÃO VERÁ O NANAMI DE CUECA, APENAS A SALVAÇÃO</h1>;
 }
 
 export default Home;
